bd4992d566b496eb943562075c54d7e65a5d5f49
Tommaso <p.tommy93@gmail.com>
2022-07-03 14:11:02 +0200

    chore: Add build instructions

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cargo build --target wasm32-unknown-emscripten --release
+mkdir -p lib
+cp target/wasm32-unknown-emscripten/release/mozjpeg-wasm.js lib/mozjpeg-wasm.js
+cp target/wasm32-unknown-emscripten/release/mozjpeg_wasm.wasm lib/mozjpeg-wasm.wasm
