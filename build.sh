9f9e2062365c97f28c8f43669b00f3ae5fb523d5
VJK <karthikeyanvj@gmail.com>
2023-12-23 16:40:06 +0200

    fix wasm file name in lib folder

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cargo build --target wasm32-unknown-emscripten --release
 mkdir -p lib
 cp target/wasm32-unknown-emscripten/release/mozjpeg-wasm.js lib/mozjpeg-wasm.js
-cp target/wasm32-unknown-emscripten/release/mozjpeg_wasm.wasm lib/mozjpeg-wasm.wasm
+cp target/wasm32-unknown-emscripten/release/mozjpeg_wasm.wasm lib/mozjpeg_wasm.wasm
